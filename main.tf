a41f7725ba789e6c56bff0eafeb9e5148083bca8
Erik Andreazi <andreazi.erik@gmail.com>
2024-07-13 19:49:11 -0300

    Iniciando os modulos data e rds, difinindo algumas variaveis

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
-data "aws_subnets" "public-subnets" {
-  filter {
-    name   = "tag:Name"
-    values = ["Public-*"]
-  }
+module "data" {
+  source = "./modules/data_value"
+
+  default-vpc-name   = var.default-vpc-name
+  public-subnet-name = var.public-subnet-name
+  private-vpc-name   = var.private-vpc-name
+}
+
+module "rds" {
+  source = "./modules/rds"
+
+  # global
+  projeto         = var.projeto
+  vpc_id          = module.data.vpc-id
+  private_subnets = module.data.private-subnet
+
+  # rds
+  db_engine         = var.db_engine
+  db_engine_version = var.db_engine_version
+  db_instance       = var.db_instance
+  db_username       = var.db_username
+  db_password       = var.db_password
+
+  depends_on = [module.data]
 }
